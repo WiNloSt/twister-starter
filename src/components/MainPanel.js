3e485fc280f64d90ef6ffc174180f194430133ba
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2017-03-16 10:18:06 +0700

    Add tweet enable toggle feature

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -4,6 +4,10 @@ import NewTweet from './NewTweet'
 import * as api from '../api'
 
 class MainPanel extends React.Component {
+  static propTypes = {
+    enableTweet: React.PropTypes.bool
+  }
+
   state = {
     tweets: [],
     username: 'kelvin',
@@ -41,11 +45,14 @@ class MainPanel extends React.Component {
   render() {
     return (
       <div className='main-panel'>
-        <NewTweet
-          name={this.state.name}
-          username={this.state.username}
-          addNewTweet={this.addNewTweet}
-        />
+        {
+          this.props.enableTweet ?
+            <NewTweet
+              name={this.state.name}
+              username={this.state.username}
+              addNewTweet={this.addNewTweet}
+            /> : ''
+        }
         <TweetList tweets={this.state.tweets} />
       </div>
     )
